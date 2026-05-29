0df579d518c2c45064bde595a1b1b7d2a4437d9c
raindelacruz <rainierjdelacruz@gmail.com>
2026-05-29 10:30:41 +0800

    Add vendor dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nfa/ict-ticketing-system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '57713a828624186a414ced46717c18b6294fa754',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nfa/ict-ticketing-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '57713a828624186a414ced46717c18b6294fa754',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
